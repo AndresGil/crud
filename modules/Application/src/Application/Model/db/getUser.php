a301730d19301e6c9d4d8010e6a645c25dc640ea
Andrés <sr.gil.arias@gmail.com>
2015-04-24 13:57:10 +0200

    hola

---
+++ modules/Application/src/Application/Model/db/getUser.php
@@ -0,0 +1,33 @@
+<?php
+
+function getUsers($config)
+{
+    //conectamos a la db
+    $link = mysqli_connect($config['$host'],
+                            $config['$user'],
+                            $config['$password'],
+                            $config['$database']
+        );
+                                
+    
+    
+    
+    
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
